b4045d0a8c26bfaa14cbf615501ccf90409127f6
thanh dan <thanhdanhk2004@gmail.com>
2025-02-22 16:34:13 +0700

    init project

---
+++ ThanhDan/Source.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+bool soNguyenTo(int soA)
+{
+    if (soA < 2)
+        return false;
+
+    for (int i = 2; i <= sqrt((float)soA); i++)
+    {
+        if (soA % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool nam_nhuan(int year)
+{
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0) {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    else if (year % 4 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main() {
+
+	return 0;
+}
